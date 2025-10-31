cf885bbb205d9e68d5a85fd11c7b520e69f6f354
jplovett <50369339+jplovett@users.noreply.github.com>
2025-10-31 07:25:33 -0400

    bootstrap/scss loaded

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import '../src/scss/styles.scss'
 import "./index.css";
 
 createApp(App).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <section>
     <h1>Welcome</h1>
     <p>Minimal Vue 3 SPA powered by Vite.</p>
+    <div class="p-2">
+      <button class="btn btn-primary mx-2">Am I dark blue?</button>
+      <button class="btn btn-success mx-2">Am I green?</button>
+    </div>
   </section>
 </template>
 
